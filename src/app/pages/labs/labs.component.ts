10973c870cf89c29fd6440727fbb735b2aa3fd51
Jesus Ramon <108687344+JesusRamon2192@users.noreply.github.com>
2024-03-27 11:22:57 +0000

    Clase 15 Controlando un input con formControl y ReactiveForms Module

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css',
 })
@@ -26,6 +26,15 @@ export class LabsComponent {
     age: 15,
     avatar: 'https://w3schools.com/howto/img_avatar.png',
   });
+
+  colorCtrl = new FormControl();
+
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
+
   clickHandler() {
     alert('hola');
   }
