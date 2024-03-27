beeccf61c11449ce9db3d9c6e95ab74274338ef3
Jesus Ramon <108687344+JesusRamon2192@users.noreply.github.com>
2024-03-27 05:53:26 +0000

    clase 6 eventos change

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,29 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
   selector: 'app-labs',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
 export class LabsComponent {
   welcome = 'hola';
-  tasks = [
-    'Instalar el angular cli',
-    'Crear proyecto',
-    'Crear componentes'
-  ]
-  name = "jesus";
+  tasks = ['Instalar el angular cli', 'Crear proyecto', 'Crear componentes'];
+  name = 'jesus';
   age = 32;
   disabled = true;
   img = 'https://w3schools.com/howto/img_avatar.png';
 
-  person= {
-    name:'Nicolas',
-    age:18,
-    avatar: 'https://w3schools.com/howto/img_avatar.png'
+  person = {
+    name: 'Nicolas',
+    age: 18,
+    avatar: 'https://w3schools.com/howto/img_avatar.png',
+  };
+  clickHandler() {
+    alert('hola');
+  }
+
+  changeHandler(event: Event) {
+    console.log(event);
+  }
+
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
+
+  valueInput = '';
+
+  changeTextKeyboard(event: KeyboardEvent) {
+    const elementInput = event.target as HTMLInputElement;
+    this.valueInput = elementInput.value;
   }
 }
