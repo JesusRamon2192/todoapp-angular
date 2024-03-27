4012cfaf810d419d76d7ad339875a2a5b62927f8
Jesus Ramon <108687344+JesusRamon2192@users.noreply.github.com>
2024-03-27 08:09:01 +0000

    clase 11 ngfor para objetos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Task } from './../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -10,21 +10,47 @@ import { signal } from '@angular/core';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  tasks = signal([
-    'Instalar el angular cli',
-    'Crear proyecto',
-    'Crear componentes',
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Learn Angular',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componentes',
+      completed: false,
+    },
   ]);
+
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
-    this.tasks.update((tasks) => [...tasks, newTask]);
     input.value = '';
+    this.addTask(newTask);
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+    this.tasks.update((tasks) => [...tasks, newTask]);
   }
 
   deleteTask(index: number) {
     this.tasks.update((tasks) =>
       tasks.filter((task, position) => position !== index),
     );
+  }
+
+  toggleChecked(index: number) {
+    this.tasks.update((value) => {
+      return value.map((task, position) => {
+        if (position === index) return { ...task, completed: !task.completed };
+        return task;
+      });
+    });
   }
 }
