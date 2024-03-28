fc2b0cff4969f60ac6de5fba268843db8cfe3037
JesusRamon2192 <jesus.ramon2192@gmail.com>
2024-03-27 22:46:59 -0600

    Clase 19, clase edicion

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -54,18 +54,52 @@ export class HomeComponent {
 
   deleteTask(index: number) {
     this.tasks.update((tasks) =>
-      tasks.filter((task, position) => position !== index),
+      tasks.filter((task, position) => 
+      position !== index),
     );
   }
 
   toggleChecked(index: number) {
     this.tasks.update((tasks) => {
       return tasks.map((task, position) => {
-        if (position === index) return { ...task, completed: !task.completed };
+        if (position === index) 
+        return { 
+        ...task, completed: !task.completed };
         return task;
       });
     });
   }
 
-  updateTask(index: number) {}
+  updateTaskEditingMode(index: number) {
+    this.tasks.update(tasks => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            editing: true
+          }
+        }
+        return {
+          ...task,
+          editing:false
+        };
+      })
+    })
+  }
+
+  updateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tasks.update(tasks => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            title: input.value,
+            editing:false
+          }
+        }
+        return task;
+      })
+    })
+  }
 }
